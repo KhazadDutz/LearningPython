16a6a762871cab7ddda8923c21407ce3c3e8e9b2
Mauricio <mauricio.s.ieiri@gmail.com>
2023-04-13 16:55:53 -0300

    Entendendo diferenca entre comentário e DocString

---
+++ aula1.py
@@ -0,0 +1,14 @@
+# Permite escrever um comentario
+
+#imprimindo algo na tela
+print(123)
+
+
+"""
+Permite escrever um DocString
+Também serve para escrever suas notas
+"""
+
+'''
+Isso também é uma DocString
+'''
