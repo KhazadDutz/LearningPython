adbb1ea9e90ad207839d38e25572afc9b885eb10
Mauricio <mauricio.s.ieiri@gmail.com>
2023-04-13 17:16:26 -0300

    Entendendo tipos no Python: String

---
+++ aula3.py
@@ -0,0 +1,27 @@
+"""
+Python = Linguagem de programacao
+Tipo de tipagem = Dinamica / Forte
+str -> string -> texto
+String sao textos que estao dentro de aspas
+"""
+
+print(1234)
+
+# Aspas simples
+print('Khazad Dutz')
+
+# Aspas duplas
+print("Khazad Dutz")
+
+# Escape
+print('Khazad \"Dutz')
+print('Khazad \"Dutz\"')
+
+# r
+print(r"Khazad \"Dutz")
+
+# Aspas simples dentro de Aspas duplas
+print("Khazad 'Dutz'")
+
+# Aspas duplas dentro de Aspas simples
+print('Khazad "Dutz"')
