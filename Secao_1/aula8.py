419d81a63c8678aadb5d12792fa0dd32bad0dcb7
Mauricio Ieiri <mauricio.s.ieiri@gmail.com>
2023-04-28 16:36:23 -0300

    Exercício sobre variáveis

---
+++ Secao_1/aula8.py
@@ -0,0 +1,13 @@
+nome = 'Mauricio'
+sobrenome = 'Ieiri'
+idade = 29
+ano_nascimento = 1993
+maior_de_idade = idade >= 18
+altura_metros = 1.66
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maio de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
