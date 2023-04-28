801e28075eb73680d49dc5d3c81ae77a269cdcd8
Mauricio Ieiri <mauricio.s.ieiri@gmail.com>
2023-04-28 17:16:42 -0300

    Exercício de programação - Calculo do IMC

---
+++ Secao_1/aula12.py
@@ -0,0 +1,9 @@
+nome = 'KhazadDutz'
+altura = 1.66
+peso = 72
+imc = round(peso / altura ** 2, 2)
+
+#imc = peso / (alturaxaltura)
+#KhazadDutz tem 1.80 de altura, pesa 72 quilos e seu IMC é ...
+
+print(nome + ' tem ' + str(altura) + ' de altura, pesa ' + str(peso) + ' quilos e seu IMC é ' + str(imc))
