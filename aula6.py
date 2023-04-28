589d60bc6411c5abcecae7aec4dc4e5ec9863ef2
Mauricio Ieiri <mauricio.s.ieiri@gmail.com>
2023-04-28 13:48:19 -0300

    aprendendo sobre conversão de tipos

---
+++ aula6.py
@@ -0,0 +1,15 @@
+# Conversão de tipos, coerção, type convertion, typecasting, corecion
+# é o ato de converter um tipo em outro.
+# Tipos imutáveis e primitivos:
+# str, int, float, bool
+
+print(1 + 1) # 2
+print('a' + 'b') # 'ab'
+
+# print('1' + 1) #TypeError
+
+print(int('1') + 1) # 2
+print(float('1') + 1) # 2.0
+print(bool(' '))
+print(bool(''))
+print(str(29) + 'b')
